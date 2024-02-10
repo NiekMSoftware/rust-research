631e00531d87d5b08466f50c8d1f58a9140d0cf4
Niek Melet <niek+github@meletstudios.com>
2024-02-10 14:51:58 +0100

    Finished Guessing Game
    Finished the guessing game and added a few more adjustments to it.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,39 @@
+use std::cmp::Ordering;
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please enter your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    // run the  loop
+    loop {
+        println!("Please enter your guess.");
+        eprint!("> ");
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line.");
-    println!("You guessed: {guess}");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        // parse string value to integer
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("\nERR: \nOnly accepting numbers!\n");
+                continue;
+            },
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Your guess: {guess}, is too small!"),
+            Ordering::Greater => println!("Your guess: {guess}, is too big!"),
+            Ordering::Equal => {
+                println!("Your guess: {guess}, was correct! Good job!");
+                break;
+            }
+        }
+    }
+
+    // display final message
+    println!("Ending application! \nSee you later!~\n")
 }
